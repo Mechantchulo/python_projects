16c39b5399e991ea90b1821c38be7ba05792a9e5
Erick Mutua <108677764+Mechantchulo@users.noreply.github.com>
2024-01-13 02:57:03 +0300

    Update student details.py

--- student details.py
+++ student details.py
@@ -1,3 +1,4 @@
+#prompting the user to input their details.
 First_name = input("What is your name: ")
 Second_name = input("What is your second name: ")
 Faculty_name = input("What faculty are you in: ")
@@ -6,4 +7,5 @@ course_name = input("What course are you studying: ")
 Student_number = input("Registration number: ")
 Age = float(input("Type in your age: "))
 
+#displaying the student details
 print(f"Hello { First_name } { Second_name } REG.NO { Student_number }.Welcome to the  { Faculty_name } and congratulations for choosing { course_name }.")
